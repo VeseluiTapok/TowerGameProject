a890dd26ce72ddb4be852d5c34219b6fc8c2f08b
VeseluiTapok <162804936+VeseluiTapok@users.noreply.github.com>
2024-09-11 12:30:25 -0700

    доробив FPS

--- src/com/TowerGame/GameLauncher.java
+++ src/com/TowerGame/GameLauncher.java
@@ -1,8 +1,23 @@
 package com.TowerGame;
 
+import javax.swing.*;
+
 public class GameLauncher {
 
     public static void main(String[] args) {
-        new Window();
+        JFrame window = new JFrame();
+        window.setTitle("TowerGame");
+        window.setResizable(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        GamePanel panel = new GamePanel();
+        window.add(panel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        panel.startGameThread();
     }
 }
